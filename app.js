ff0645575a7504deab9f4765054b09a65fd2dafc
Alex Vogt <alexvogt18@gmail.com>
2021-04-13 16:25:24 -0700

    lab 6 complete

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const cookieHours = ['6am', '7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
-
+// First array is store hours, next is the only Object, after that is sales array then render
 const seattle = {
   cityName: 'Seattle',
   minCustomer: 23,
@@ -13,3 +13,40 @@ const seattle = {
   }
 }
 
+function generateSaleArray(location){
+  location.grandTotal = 0;
+  location.hourlySaleArray = [];
+  for (let i = 0; i < cookieHours; i++){
+    let customers = location.randoCustnum();
+    let cookiesSold = Math.floor(customers * location.avgCookpCust);
+    location.grandTotal += cookiesSold;
+    location.hourlySalesArray.push(cookiesSold);
+  }
+}
+
+generateSaleArray(seattle);
+
+const locationDivElem = document.getElementById('locations');
+
+function renderStoreInfo(location) {
+  const h2Elem = document.createElement('h2');
+  h2Elem.textContent = location.cityName;
+  locationDivElem.appendChild(h2Elem);
+//   h2 is rendered, now ul and li need to render
+  const ulElem = document.createElement('ul');
+  locationDivElem.appendChild(ulElem);
+  for (let i = 0; i < cookieHours.length; i++) {
+    const liElem = document.createElement('li');
+    liElem.textContent = '${cookieHours[i]}: ${location.hourlySaleArray[i]} cookies';
+    ulElem.appendChild(liElem);
+  }
+}
+
+const totalLiElem = document.createElement('li');
+
+totalLiElem.textContent = 'Total: ' + location.grandTotal + ' cookies';
+
+ulElem.appendChild(totalLiElem);
+}
+
+renderLocationInfo(seattle);
