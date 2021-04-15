6ff8c554f22d0f4d1f813a833bc345c50f09954f
Alex Vogt <alexvogt18@gmail.com>
2021-04-14 21:56:05 -0700

    Updates Salmon Cookies with Table

--- app.js
+++ app.js
@@ -50,6 +50,10 @@
 
 // renderStoreInfo(seattle);
 
+// *** PART 2 CONSTRUCTOR FUNCTIONS ***
+
+const cookieHours = ['6am', '7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+
 function StorePlace(cityName, minCustomer, maxCustomer, avgCookpCust) {
   this.name = cityName;
   this.minCustomer = minCustomer;
@@ -57,3 +61,95 @@ function StorePlace(cityName, minCustomer, maxCustomer, avgCookpCust) {
   this.avgCookpCust = avgCookpCust;
 }
 
+function randoCustnum(this.minCustomer, this.maxCustomer) {
+  return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer);
+}
+
+StorePlace.prototype.getAvgcust = function() {
+  this.avgCustnum = randoCustnum(2,65);
+}
+
+StorePlace.prototype.generateSalesArray = function() {
+  location.grandTotal = 0;
+  location.hourlySalesArray = [];
+  for (let i = -1; i < cookieHours.length; i++){
+    let customers = location.randoCustnum();
+    let cookiesSold = Math.floor(customers * this.avgCookpCust);
+    location.grandTotal += cookiesSold;
+    location.hourlySalesArray.push(cookiesSold);
+  }
+}
+
+const seattle = new StorePlace('Seattle', 23, 65, 6.3);
+
+const tokyo = new StorePlace('Tokyo', 3, 24, 1.2);
+
+const dubai = new StorePlace('Dubai', 11, 38, 3.7);
+
+const paris = new StorePlace('Paris', 20, 38, 2.3);
+
+const lima = new StorePlace('Lima', 2, 16, 4.6);
+
+seattle.getAvgcust();
+tokyo.getAvgcust();
+dubai.getAvgcus();
+paris.getAvgcust();
+lima.getAvgcus();
+
+const storeDivElem = document.getElementById('storeNames');
+
+StorePlace.prototype.render = function() {
+  // h2 element create
+  const h2Elem = document.createElement('h2');
+  h2Elem.textContent = 'Cookies Sold per Open Hour';
+  locationDivElem.appendChild(h2Elem);
+  // p element create
+  const pElem = document.createElement('p');
+  locationDivElem.appendChild(pElem);
+  // table element creat
+  const tableElem = document.createElement('table');
+  storeDivElem.appendChild(tableElem);
+  //First Table Row
+  const trElem = document.createElement('tr')
+  tableElem.appendChild(trElem);
+  //First Table Heading
+  constthElem1 = document.createElement('th');
+  thElem1.textContent = 'STORE NAME';
+  trElem.appendChild(thElem1);
+  //Second Table Heading
+  const thElem2 = document.createElement('th');
+  thElem2.textContent = 'MIN. CUSTOMER'
+  trElem.appendChild(thElem2);
+  //Third Table Heading
+  const thElem3 = document.createElement('th');
+  thElem3.textContent = 'MAX. CUSTOMER'
+  trElem.appendChild(thElem3);
+  //Fourth Table Heading
+  const thElem4 = document.createElement('th');
+  thElem4.textContent = 'AVG. CUSTOMER'
+  //Start Next Row to insert Data into Table
+  const trElem2 = document.createElement('tr');
+  tableElem.appendChild(trElem2);
+  // First Data Cell
+  const tdElem1 = document.createElement('td');
+  tdElem1.textContent = this.name;
+  trElem2.appendChild(tdElem1);
+  // Second Data Cell
+  const tdElem12 = document.createElement('td');
+  tdElem2.textContent = this.minCustomer;
+  trElem2.appendChild(tdElem2);
+  // Third Data Cell
+  const tdElem3 = document.createElement('td');
+  tdElem3.textContent = this.maxCustomer;
+  trElem2.appendChild(tdElem1);
+  // Fourth Data Cell
+  const tdElem1 = document.createElement('td');
+  tdElem1.textContent = this.avgCookpCust;
+  trElem2.appendChild(tdElem1);
+}
+
+seattle.render();
+tokyo.render();
+dubai.render();
+paris.render();
+lima.render();
